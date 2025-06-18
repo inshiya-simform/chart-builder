7e444171e2813c56478a994ba6ae3f1ad75a93da
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-06-18 14:16:11 +0530

    feature: chart

--- src/component/Content/Content.tsx
+++ src/component/Content/Content.tsx
@@ -1,5 +1,28 @@
+import type { ChartData } from "chart.js";
+import { Charts } from "lune-ui";
+
+const chartData: ChartData = {
+  labels: ["January", "February", "March", "April", "May"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [10, 20, 30, 40, 50],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)"],
+      borderWidth: 1,
+    },
+  ],
+};
 const Content = () => {
-  return <div>content</div>;
+  return (
+    <Charts
+      className="chart"
+      chartType="line"
+      chartTitle="Sample Chart"
+      chartLegend="bottom"
+      data={chartData}
+    />
+  );
 };
 
 export default Content;
